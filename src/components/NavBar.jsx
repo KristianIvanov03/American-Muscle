6ecf413c3cee50430f7a2cdfe76e8d5afcabdd0e
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-22 09:37:28 +0200

    added nav bar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>American Muscle</h1>
+            </div>
+            <div className="links">
+                <ul>
+                    <li><h4>Info</h4></li>
+                    <li><h4>Our Mission</h4></li>
+                    <li><h4>Catalog</h4></li>
+                    <li><h4>Rate us</h4></li>
+                    <li><h4>Contacts</h4></li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+ 
+export default NavBar ;
