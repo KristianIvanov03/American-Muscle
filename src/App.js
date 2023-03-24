526217c2014221c8261300533d1eee7f85be9848
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-24 17:38:25 +0200

    imported info into app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Info from "./components/Info";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Info/>
     </div>
   );
 }
