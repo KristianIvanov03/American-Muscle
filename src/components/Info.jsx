9c300781f06708e186600dea7414f6b75b906995
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-24 17:37:11 +0200

    basic html elements added to info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,20 @@
 const Info = () => {
     return ( 
-        <>
-        </>
+        <section>
+            <div>
+                <div>
+                    <div>
+                        <h1>What is American Muscle</h1>
+                    </div>
+                    <div>
+                        <p>American Muscle is a place where you can see a full catalog with all of the American muscle cars in one place. Our mission is to give you full access to all american muscles cars with all of their specs to make your research more interactive and accessible and help you to choose your dream car.</p>
+                    </div>
+                </div>
+                <div>
+                    <img src="https://res.cloudinary.com/dkmrw3av4/image/upload/v1679502997/american%20muscle/300634433_10160258209459224_8682622498202919289_n_kdw09r.jpg" alt="3 dodge cars"/>
+                </div>
+            </div>
+        </section>
      );
 }
  
