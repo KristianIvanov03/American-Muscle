9f980074b71fa87d7d84014fc8fe1066e9920b87
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-24 18:17:46 +0200

    added css for info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,17 +1,19 @@
+import '../assets/info.css';
+
 const Info = () => {
     return ( 
         <section>
-            <div>
-                <div>
-                    <div>
+            <div className="container">
+                <div className="text">
+                    <div className="heading">
                         <h1>What is American Muscle</h1>
                     </div>
-                    <div>
+                    <div className="paragraph">
                         <p>American Muscle is a place where you can see a full catalog with all of the American muscle cars in one place. Our mission is to give you full access to all american muscles cars with all of their specs to make your research more interactive and accessible and help you to choose your dream car.</p>
                     </div>
                 </div>
-                <div>
-                    <img src="https://res.cloudinary.com/dkmrw3av4/image/upload/v1679502997/american%20muscle/300634433_10160258209459224_8682622498202919289_n_kdw09r.jpg" alt="3 dodge cars"/>
+                <div className="image">
+                    <img src="https://res.cloudinary.com/dkmrw3av4/image/upload/v1679674112/american%20muscle/Screenshot_2023-03-24_180806_ncadat.jpg" alt="3 dodge cars"/>
                 </div>
             </div>
         </section>
