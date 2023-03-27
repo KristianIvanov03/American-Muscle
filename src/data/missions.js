050175cd88ba1035534c6258362d920e1113df4f
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-27 17:43:12 +0300

    added data

---
+++ src/data/missions.js
@@ -0,0 +1,17 @@
+export const missions = [
+    {
+        icon: 'bi bi-database-fill-lock',
+        title: 'Full access',
+        desc: 'Giving you a full access to all american muscle cars in one place'
+    },
+    {
+        icon: 'bi bi-binoculars-fill',
+        title: 'Interactive research',
+        desc: 'Making your research more interactive and accessible '
+    },
+    {
+        icon: 'bi bi-emoji-sunglasses-fill',
+        title: 'Help you',
+        desc: 'Help you to choose your dream car in a couple of clicks'
+    }
+];
