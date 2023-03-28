ba0486c6eaf0a14a1ad96340221ac9eb27c0aa46
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-28 16:16:34 +0300

    css changes

--- src/components/OurMission.jsx
+++ src/components/OurMission.jsx
@@ -1,19 +1,23 @@
 import { missions } from "../data/missions";
+import '../assets/missions/missions.css';
 
 const OurMission = () => {
     return (
-        <div className="missions">
-            {
-                missions.map((mission) => {
-                    return (
-                        <div key={mission.id} className="mission" id={mission.id}>
-                            <img src={mission.icon} alt="icon"/>
-                            <h1>{mission.title}</h1>
-                            <p>{mission.desc}</p>
-                        </div>
-                    )
-                })
-            }
+        <div className="container1">
+            <h1 className="title1">Our Missions</h1>
+            <div className="missions">
+                {
+                    missions.map((mission) => {
+                        return (
+                            <div key={mission.id} className="mission" id={mission.css}>
+                                <img src={mission.icon} alt="icon"/>
+                                <h2>{mission.title}</h2>
+                                <p>{mission.desc}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 }
--- src/data/missions.js
+++ src/data/missions.js
@@ -1,18 +1,21 @@
 export const missions = [
     {
         id: 1,
+        css: "id1",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007576/american%20muscle/key-outline_bnkl6s.svg',
         title: 'Full access',
         desc: 'Giving you a full access to all american muscle cars in one place'
     },
     {
         id: 2,
+        css: "id2",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007583/american%20muscle/search-circle-outline_rwpb7y.svg',
         title: 'Interactive research',
         desc: 'Making your research more interactive and accessible '
     },
     {
         id: 3,
+        css: "id3",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007563/american%20muscle/information-circle-outline_j7895b.svg',
         title: 'Help you',
         desc: 'Help you to choose your dream car in a couple of clicks'
