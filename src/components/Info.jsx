d502a1069c0201abd0cea3bb7fb7d00a4e569adb
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-30 19:11:45 +0300

    changed info text description

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -9,7 +9,7 @@ const Info = () => {
                         <h1>What is American Muscle</h1>
                     </div>
                     <div className="paragraph">
-                        <p>American Muscle is a place where you can see a full catalog with all of the American muscle cars in one place. Our mission is to give you full access to all american muscles cars with all of their specs to make your research more interactive and accessible and help you to choose your dream car.</p>
+                        <p>Welcome to American Muscle – the premier online destination for showcasing the latest and greatest American muscle cars. Our website is dedicated to highlighting the most powerful and stylish muscle cars from all the top American automakers. If you're looking for the ultimate guide to the newest muscle cars on the market, look no further than American Muscle!</p>
                     </div>
                 </div>
                 <div className="image">
