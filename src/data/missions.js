7a9045e0c39cf999fabd32e19cd95502c8e14306
Your Name <kristian.ivanov.pr@gmail.com>
2023-03-30 20:56:18 +0300

    changes

--- src/data/missions.js
+++ src/data/missions.js
@@ -4,20 +4,20 @@ export const missions = [
         css: "id1",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007576/american%20muscle/key-outline_bnkl6s.svg',
         title: 'Full access',
-        desc: 'Giving you a full access to all american muscle cars in one place'
+        desc: 'Provide you with full access to the newest American muscle cars'
     },
     {
         id: 2,
         css: "id2",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007583/american%20muscle/search-circle-outline_rwpb7y.svg',
         title: 'Interactive research',
-        desc: 'Making your research more interactive and accessible '
+        desc: 'Compare models, customize your dream car, and see the impact of upgrades. Join us today!'
     },
     {
         id: 3,
         css: "id3",
         icon: 'https://res.cloudinary.com/dkmrw3av4/image/upload/v1680007563/american%20muscle/information-circle-outline_j7895b.svg',
         title: 'Help you',
-        desc: 'Help you to choose your dream car in a couple of clicks'
+        desc: 'Help you find your dream muscle car with a couple of clicks'
     }
 ];
